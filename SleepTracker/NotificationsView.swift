395aa0cfc55c646defc30b163637bc643b4361ee
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2025-03-09 23:54:07 -0400

    fixed "Save" button

--- SleepTracker/NotificationsView.swift
+++ SleepTracker/NotificationsView.swift
@@ -1,5 +1,5 @@
 //
-//  SettingsView.swift
+//  NotificationsView.swift
 //  SleepTracker
 //
 //  Created by Timur on 2/14/25.
@@ -12,6 +12,7 @@ struct NotificationsView: View {
     @AppStorage("customNotificationTime") private var customNotificationTimeString: String = ""
     
     @State private var selectedNotificationTime: Date = Date()
+    @State private var isTimeManuallyUpdated: Bool = false
     
     var body: some View {
         NavigationView {
@@ -19,62 +20,71 @@ struct NotificationsView: View {
                 LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.purple.opacity(0.8)]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .edgesIgnoringSafeArea(.all)
                 
-                Form {
-                    Section() {
-                        Toggle("Enable Notifications", isOn: $notificationsEnabled)
-                            .foregroundColor(.primary)
-                            .padding(.horizontal)
-                        
-                        if notificationsEnabled {
-                            VStack(alignment: .center, spacing: 20) {
-                                Button(action: {
-                                    selectedNotificationTime = loadNotificationTime()
-                                }) {
-                                    Text("Calculated best Bedtime: \(formattedNotificationTime)")
-                                        .font(.subheadline)
-                                        .foregroundColor(.blue)
-                                }
-                                .padding()
-                                .background(Color.white)
-                                .cornerRadius(10)
-                                .shadow(radius: 5)
-                                
-                                DatePicker("Notification Time", selection: $selectedNotificationTime, displayedComponents: .hourAndMinute)
-                                    .datePickerStyle(WheelDatePickerStyle())
-                                    .labelsHidden()
-                                    .frame(height: 150)
+                VStack {
+                    Form {
+                        Section {
+                            Toggle("Enable Notifications", isOn: $notificationsEnabled)
+                                .foregroundColor(.primary)
+                                .padding(.horizontal)
+                            
+                            if notificationsEnabled {
+                                VStack(alignment: .center, spacing: 20) {
+                                    Button(action: {
+                                        selectedNotificationTime = loadNotificationTime()
+                                    }) {
+                                        Text("Calculated best Bedtime: \(formattedNotificationTime)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.blue)
+                                    }
+                                    .padding()
                                     .background(Color.white)
                                     .cornerRadius(10)
                                     .shadow(radius: 5)
-                                    .padding(.horizontal)
-                                
-                                Button(action: {
-                                    saveNotificationTime()
-                                }) {
-                                    Text("Save")
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .frame(maxWidth: 150)
-                                        .background(Gradient(colors: [.blue.opacity(0.8), .purple.opacity(0.8)]))
+                                    
+                                    DatePicker("Notification Time", selection: $selectedNotificationTime, displayedComponents: .hourAndMinute)
+                                        .datePickerStyle(WheelDatePickerStyle())
+                                        .labelsHidden()
+                                        .frame(height: 150)
+                                        .background(Color.white)
                                         .cornerRadius(10)
                                         .shadow(radius: 5)
+                                        .padding(.horizontal)
+                                        .onChange(of: selectedNotificationTime) { _ in
+                                            isTimeManuallyUpdated = true
+                                        }
                                 }
-                                .padding(.horizontal)
+                                .padding(.vertical)
                             }
-                            .padding(.vertical)
                         }
                     }
+                    .padding(.top)
+                    .background(Color.white.opacity(0.9))
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                    
+                    if notificationsEnabled {
+                        Button(action: {
+                            saveNotificationTime()
+                        }) {
+                            Text("Save")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(maxWidth: 200)
+                                .background(LinearGradient(gradient: Gradient(colors: [.blue.opacity(0.8), .purple.opacity(0.8)]), startPoint: .leading, endPoint: .trailing))
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        .padding(.top)
+                    }
                 }
-                .padding(.top)
-                .background(Color.white.opacity(0.9))
-                .cornerRadius(20)
-                .shadow(radius: 10)
             }
             .navigationTitle("Notifications")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
-                selectedNotificationTime = loadNotificationTime()
+                if !isTimeManuallyUpdated {
+                    selectedNotificationTime = loadNotificationTime()
+                }
             }
         }
     }
